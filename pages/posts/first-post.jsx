5bbacd31e12c45362d747dd80093591887add9c3
Vignnesh <ravin.vignnesh@gmail.com>
2022-10-20 09:26:05 +0800

    Adding Head and Script tags

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,12 +1,22 @@
 import Link from "next/link"
+import Head from "next/head"
+import Script from "next/script"
 
 export default function FirstPost(){
     return (
         <>
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">Back to home</Link>
-            </h2>
+        <Head>
+            <title>First post</title>
+        </Head>
+        <Script 
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnload"                                                                           // Load script lazily during browser idle time
+            onLoad={() => console.log("Script was loaded correctly, window.FB has been populated")}        // Function to run immediately after loading script
+        />
+        <h1>First Post</h1>
+        <h2>
+            <Link href="/">Back to home</Link>
+        </h2>
         </>
     )
 }
